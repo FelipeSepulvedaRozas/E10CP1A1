44f9a204eebafa8cbbdb98ff34cfed9aacf95a01
FelipeSepulvedaRozas <felipe.s.rozas@gmail.com>
2018-11-12 14:02:30 -0300

    activity completed but not upload to github

--- exercise6.rb
+++ exercise6.rb
@@ -1,13 +1,47 @@
-class student
-
-	def person(name, note1 =4,note2 =4)
-		@name = name
-		@note1 = note1
-		@note2 = note2
-		quantity +=
-
-
-	end
-
-
+module Test
+  def result
+    sum = @grade1 + @grade2
+    avr = sum/2
+    if avr > 4 then
+      p "#{name}: Estudiante aprobado"
+    else
+      p "#{name}: Estudiante reprobado"
+    end
+  end
 end
+
+module Attendance
+  def student_quantity
+  p "La cantidad de alumnos en el curso es #{quantity} "
+  end
+end
+class Student
+  extend Attendance
+  include Test
+  @grade1 = 4
+  @grade2 = 4
+  @@quantity = 0
+attr_accessor :name, :grade1, :grade2
+  def initialize(name, grade1, grade2)
+    @name = name
+    @grade1 = grade1
+    @grade2= grade2
+    @@quantity +=1
+  end
+  def self.quantity
+    @@quantity
+  end
+end
+
+Student.new('Alexis', 7, 6).result
+Student.new('Roberto', 5, 5).result
+Student.new('Claudio', 2.4, 3.4).result
+Student.new('Felipe', 4.5, 5.5).result
+Student.new('Alejandro', 1, 3).result
+Student.new('Javier', 5.4, 6.2).result
+Student.new('David', 7, 6.5).result
+Student.new('Sebastian', 2.5, 1.3).result
+Student.new('Elias', 7, 6.5).result
+Student.new('Nicolas', 7.9, 6.0).result
+
+Student.student_quantity
